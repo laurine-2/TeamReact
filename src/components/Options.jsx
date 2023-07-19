6108a2ff5db57bcb5b88afa08b99fa6fe87830c9
laurine-2 <laurinembogning2000@gmail.com>
2023-07-20 00:14:15 +0200

    first commit

---
+++ src/components/Options.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button, TextField, Grid, Typography, Container, Paper } from '@material-ui/core';
+import  { makeStyles } from '@material-ui/core/styles';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
+const Options = ({ children }) => {
+  return (
+    <div>
+      Options
+      {children}
+    </div>
+  )
+}
+
+export default Options
